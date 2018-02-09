bdfd90e828d85fd23e7c3e76e154768c2151f5ac
Jaric Thorning <jaric.thorning@uqconnect.edu.au>
2018-02-09 22:20:33 +0100

    Added graph to user activity

--- app/src/main/java/redjthorn/slacklog/UserActivity.java
+++ app/src/main/java/redjthorn/slacklog/UserActivity.java
@@ -1,5 +1,6 @@
 package redjthorn.slacklog;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,11 +8,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.ValueDependentColor;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.Series;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 public class UserActivity extends AppCompatActivity {
 
+    private final UserActivity thisActivity = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -26,7 +48,6 @@ public class UserActivity extends AppCompatActivity {
 
         String userId;
 
-
         if(extras == null){
             userNameText.setText("No User");
         } else {
@@ -36,6 +57,95 @@ public class UserActivity extends AppCompatActivity {
             //FILL:
             userNameText.setText(userId);
         }
+
+
+        //TODO: Fill with user login logs
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+
+        // generate Dates
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.add(Calendar.DATE, -100);
+
+        Date start = calendar.getTime();
+
+        List<DataPoint> dataPoints = new ArrayList<>();
+        Random random = new Random();
+
+        for(int i = 0; i < 100; i++){
+            dataPoints.add(new DataPoint(calendar.getTime(), random.nextInt(20)));
+            calendar.add(Calendar.DATE, 1);
+        }
+
+        Date end = calendar.getTime();
+
+        // you can directly pass Date objects to DataPoint-Constructor
+        // this will convert the Date to double via Date#getTime()
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(dataPoints.toArray(new DataPoint[100]));
+        graph.addSeries(series);
+
+        // styling
+        /*series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
+            @Override
+            public int get(DataPoint data) {
+                return Color.rgb((int) data.getX() * 255 / 4, (int) Math.abs(data.getY() * 255 / 6), 100);
+            }
+        });*/
+
+        series.setSpacing(10);
+
+        // draw values on top
+        series.setDrawValuesOnTop(false);
+        series.setValuesOnTopColor(Color.RED);
+        //series.setValuesOnTopSize(50);
+
+        //--- END ---
+
+        // set date label formatter
+//        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(1); // only 4 because of the space
+//        graph.getGridLabelRenderer().setHorizontalLabelsAngle(90);
+
+        // use static labels for horizontal and vertical labels
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        staticLabelsFormatter.setHorizontalLabels(new String[]{"100 days", "50 days", "today"});
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+
+
+
+        // set manual x bounds to have nice steps
+        graph.getViewport().setMinX(start.getTime());
+        graph.getViewport().setMaxX(end.getTime());
+        graph.getViewport().setXAxisBoundsManual(true);
+
+
+        // as we use dates as labels, the human rounding to nice readable numbers
+        // is not necessary
+        graph.getGridLabelRenderer().setHumanRounding(true);
+
+
+
+        graph.getGridLabelRenderer().setHorizontalAxisTitle("Date");
+        graph.getGridLabelRenderer().setVerticalAxisTitle("Logins");
+
+        graph.getGridLabelRenderer().setLabelHorizontalHeight(10);
+
+        //Make graph scrollable and scalable
+        //graph.getViewport().setScalable(true);
+        graph.getViewport().setScalableY(true);
+        graph.getViewport().setScrollable(true);
+        graph.getViewport().setScrollableY(true);
+
+
+
+        series.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                Toast.makeText(thisActivity, "Series1: On Data Point clicked: " + dataPoint, Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 
